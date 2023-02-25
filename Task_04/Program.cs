4f7851059f766967005b8d38551d44f13e86be46
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-25 20:52:01 +0300

    task_04 added

---
+++ Task_04/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив из 10 случайных чисел в диапазоне от 1 до 200. 
+//Найдите количество  двузначных элементов массива. 
+// Пример для массива из 5, а не 10 элементов. В своём решении сделайте для 10
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] MyArray(int length,int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rnd = new Random(); 
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int SumTwoDigit(int[] array)
+{
+    int countdigit = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 9 && array[i] < 100) countdigit++;
+    }
+   return countdigit;
+}
+
+int size = 10;
+int minValue = 1;
+int maxValue = 200;
+int[] myArray = MyArray(size, minValue, maxValue);
+PrintArray(myArray);
+Console.WriteLine($"Колличество двухзначных элементов массива равно {SumTwoDigit(myArray)}");
