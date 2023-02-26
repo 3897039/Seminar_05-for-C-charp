9cae34f7ab119e6410944dc0578e17148081eeed
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-26 14:54:47 +0300

    task_05 added

---
+++ Task_05/Program.cs
@@ -0,0 +1,41 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] MyArray(int length,int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rnd = new Random(); 
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
+
+int size = 10;
+int minValue = 1;
+int maxValue = 200;
+int[] myArray = MyArray(size, minValue, maxValue);
+PrintArray(myArray);
+
